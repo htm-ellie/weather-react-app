7e6a35fb4e3a875867e4886997beb4cd96b94351
htm-ellie <html.elitt@gmail.com>
2026-03-20 09:38:50 -0400

    added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://ellie-litt-creative.netlify.app/" Target="_blank">
+          Ellie Litt
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/htm-ellie/weather-react-app"
+          target="_blank"
+        >
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
