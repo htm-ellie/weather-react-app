bee02bcec0b1d672702b2f37edb333d9a51f02a8
htm-ellie <html.elitt@gmail.com>
2026-03-20 10:11:00 -0400

    added rel="noreferrer" to hrefs

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://ellie-litt-creative.netlify.app/" Target="_blank">
+        <a
+          href="https://ellie-litt-creative.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Ellie Litt
         </a>{" "}
         and is{" "}
         <a
           href="https://github.com/htm-ellie/weather-react-app"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on GitHub.
         </a>
