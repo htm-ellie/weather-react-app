decd7508ee1403e593b7b7b46cc7aa54af2f3b53
htm-ellie <html.elitt@gmail.com>
2026-03-20 12:35:59 -0400

    Added APi integration

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         
-        <Weather />
+        <Weather defaultCity="Key Largo" />
         <footer>
           This project was coded by{" "}
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -1,56 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              className="form-control"
-              autoFocus="on"
-            />
+export default function Weather(props) {
+    
+    const [weatherData, setWeatherData] = useState({ ready: false });
+    function handleResponse(response) {
+        console.log(response.data)
+
+        setWeatherData({
+            ready: true,
+            date: "Wednesday 10:00a",
+          temperature: response.data.temperature.current,
+          wind: response.data.wind.speed,
+          city: response.data.city,
+          description: response.data.condition.description, 
+          humidity: response.data.temperature.humidity,
+          iconUrl: "https://www.gstatic.com/weather/conditions/v1/svg/partly_cloudy_light.svg"
+        });
+        
+        
+    }
+
+    if (weatherData.ready) {
+        return (
+          <div className="Weather">
+            <form>
+              <div className="row">
+                <div className="col-9">
+                  <input
+                    type="search"
+                    placeholder="Enter a city.."
+                    className="form-control"
+                    autoFocus="on"
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    value="Search"
+                    className="btn btn-primary w-100"
+                  />
+                </div>
+              </div>
+            </form>
+            <h1>{weatherData.city}</h1>
+            <ul>
+              <li>{weatherData.date}</li>
+              <li className="text-capitalize">{weatherData.description}</li>
+            </ul>
+            <div className="row mt-3">
+              <div className="col-6">
+                <div className="clearfix">
+                  <img
+                    src={weatherData.iconUrl}
+                    alt={weatherData.description}
+                    className="float-left"
+                  />
+
+                  <span className="temperature">
+                    {Math.round(weatherData.temperature)}
+                  </span>
+                  <span className="unit">°F</span>
+                </div>
+              </div>
+              <div className="col-6">
+                <ul>
+                  <li>Humidity: {weatherData.humidity}%</li>
+                  <li>Wind: {weatherData.wind} mph</li>
+                </ul>
+              </div>
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <h1>Miami</h1>
-      <ul>
-        <li>Friday 10:30</li>
-        <li>Partly Sunny</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://www.gstatic.com/weather/conditions/v1/svg/partly_cloudy_light.svg"
-              alt="Partly Sunny"
-              className="float-left"
-            />
-            
-              <span className="temperature">69</span>
-              <span className="unit">°F</span>
-            
-          </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 71%</li>
-            <li>Wind: 12mph</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+        );
+    } else {
+        const apiKey = "84ob6879a3t305ff0c4b95aea130d3b6";
+        let city = "Miami";
+        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=imperial`;
+        axios.get(apiUrl).then(handleResponse);
+
+        return "Loading...";
+    }
+
+  
+  
 }
