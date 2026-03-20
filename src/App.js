3f90cf8edbde2a859a94ad55e62bb194e94c8754
htm-ellie <html.elitt@gmail.com>
2026-03-20 10:27:09 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://ellie-litt-creative.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Ellie Litt
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/htm-ellie/weather-react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://ellie-litt-creative.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Ellie Litt
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/htm-ellie/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
