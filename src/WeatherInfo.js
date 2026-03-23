61f72377a39d6db068d2b4a791788eca1fb54ef6
htm-ellie <html.elitt@gmail.com>
2026-03-23 13:07:07 -0400

    added new component w F/C temp conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -19,11 +20,9 @@ export default function WeatherInfo(props) {
               alt={props.data.description}
               className="float-left"
             />
-
-            <span className="temperature">
-              {Math.round(props.data.temperature)}
-            </span>
-            <span className="unit">°F</span>
+          </div>
+          <div className="TemperatureDisplay">
+            <WeatherTemperature fahrenheit={props.data.temperature} />
           </div>
         </div>
         <div className="col-6">
