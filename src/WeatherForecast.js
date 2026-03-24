4046e8f9a712bf8c15870fd675b35386a39f6d0c
htm-ellie <html.elitt@gmail.com>
2026-03-24 09:59:45 -0400

    added the icon url from the api as a placeholder for the forecast section

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,13 +1,17 @@
 import React from "react";
+import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
-          <div className="WeatherForecast-Day">Thursday</div>
-          <div>icon </div>
-          <div className="WeatherForecast-temperatures">19 10</div>
+          <div className="WeatherForecast-Day">Thu</div>
+          <div className= "WeatherForecast-icon"> <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png" /> </div>
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temp-max">19° </span>
+            <span className="WeatherForecast-temp-min">10° </span>
+          </div>
         </div>
       </div>
     </div>
