0dac23da4469bd6af8f8e3ca2fa5497e7075bde1
htm-ellie <html.elitt@gmail.com>
2026-03-26 09:29:43 -0400

    added alt description to forecast icon

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,29 +1,31 @@
 import React from "react";
 
 export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temperature.maximum);
+    return `${temperature}°`;
+  }
+  function minTemperature() {
+    let temperature = Math.round(props.data.temperature.minimum);
+    return `${temperature}°`;
+  }
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
 
-    function maxTemperature() {
-        let temperature = Math.round(props.data.temperature.maximum);
-        return `${temperature}°`;
-    }
-     function minTemperature() {
-       let temperature = Math.round(props.data.temperature.minimum);
-       return `${temperature}°`;
-     }
-     function day(){
-        let date= new Date(props.data.time * 1000);
-        let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-        let days =["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-        return days[day];
-     }
+    return days[day];
+  }
   return (
     <div>
       <div className="WeatherForecast-Day">{day()}</div>
       <div className="WeatherForecast-icon">
         {" "}
-        <img src={props.data.condition.icon_url} />{" "}
+        <img
+          src={props.data.condition.icon_url}
+          alt={props.data.condition.description}
+        />{" "}
       </div>
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temp-max">{maxTemperature()} </span>
